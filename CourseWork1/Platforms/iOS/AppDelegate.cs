3b8cf8333c83dc858a65b5007bca6d526cf27179
Shreyash <shreyashghal3@gmail.com>
2024-12-16 21:00:09 +0545

    Add project files.

---
+++ CourseWork1/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CourseWork1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
